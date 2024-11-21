2209269151849d6389a39d2847f476d70eaea6af
AeKanemoto <178535954+kanemotoae@users.noreply.github.com>
2024-11-21 16:43:29 +0900

    a

--- src/main/java/com/dmm/task/data/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dmm.task.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dmm.task.data.entity.Posts;
-
-public interface TaskRepository extends JpaRepository<Posts, Integer> {
-
-}
